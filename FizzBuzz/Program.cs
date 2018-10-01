06ad46c875f67146a5cfff2c21b51eb80afbf979
Hwasoo Lee <Hwasoo.Lee@versanthealth.com>
2018-10-01 08:02:19 -0400

    [] Add FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzRunner.FizzBuzz(20);
+        }
+    }
+
+    class FizzBuzzRunner
+    {
+        public static void FizzBuzz(int x) 
+        {
+            if ( x < 0) return;
+            for (int i = 1; i <= x; i++) {
+                if ( i % 5 == 0 && i % 3 == 0 ) {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 3 == 0) {
+                    Console.WriteLine("Buzz");
+                }
+                else {
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
